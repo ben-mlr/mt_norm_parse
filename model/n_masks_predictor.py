7c7b54750349cf57aaf9ead6f2292d37507e1b9e
ben-mlr <b.benjamin.muller@gmail.com>
2019-07-09 14:07:11 +0200

    pred_n_mask running with correct loss mask

---
+++ model/n_masks_predictor.py
@@ -0,0 +1,22 @@
+from env.importing import torch
+
+
+def pred_n_bpe(input):
+    output = torch.empty_like(input).long()
+    for ind_sent in range(input.size(0)):
+        count_1 = 1
+        for ind_word in range(input.size(1)):
+            if input[ind_sent, ind_word] == 1:
+                output[ind_sent, ind_word] = -1
+                if count_1 == 1:
+                    ind_multi_bpe = ind_word-1
+                count_1 += 1
+            elif input[ind_sent, ind_word] == 0:
+                # reached the end of the multi-bpe
+                if ind_word >= 0 and input[ind_sent, ind_word -1] == 1:
+                    output[ind_sent, ind_multi_bpe] = min(count_1, 5)
+                    count_1 = 1
+                output[ind_sent, ind_word] = 1
+            else:
+                raise(Exception("input[ind_sent, ind_word] is neither 0 nor 1 but {}".format(input[ind_sent, ind_word])))
+    return output
