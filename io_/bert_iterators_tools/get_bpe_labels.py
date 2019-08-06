word level task : fixing masking + parsing: adding heads masks

io_/bert_iterators_tools/get_bpe_labels.py
@@ -3,17 +3,25 @@ from env.importing import pdb, OrderedDict, np, torch
 from io_.dat.constants import  PAD_ID_BERT
 
 
-def get_bpe_label_word_level_task(label, batch, input_tokens_tensor, input_alignement_with_raw):
+def get_mask_input(input_tokens_tensor, use_gpu):
+    new_input = np.array(input_tokens_tensor.cpu())
+    _input_mask = [[0 if new_input[ind_sent][ind_tok] == PAD_ID_BERT else 1 for ind_tok in range(len(new_input[ind_sent]))] for ind_sent in range(len(new_input))]
+    input_mask = torch.Tensor(_input_mask).long()
+    if use_gpu:
+        input_mask = input_mask.cuda()
+
+    return input_mask
+
+
+def get_bpe_label_word_level_task(label, batch, input_tokens_tensor, input_alignement_with_raw, use_gpu):
     output_tokens_tensor = np.array(label.cpu())
     new_input = np.array(input_tokens_tensor.cpu())
     len_max = max([len(sent) for sent in new_input])
     new_input = [[inp for inp in sent] + [PAD_ID_BERT for _ in range(len_max - len(sent))] for sent
                  in new_input]
     # we mask bpe token that have been split (we don't mask the first bpe token of each word)
-    _input_mask = [[0 if new_input[ind_sent][ind_tok] == PAD_ID_BERT or
-                         input_alignement_with_raw[ind_sent][ind_tok - 1] ==
-                         input_alignement_with_raw[ind_sent][ind_tok] else 1 for ind_tok in
-                    range(len(new_input[ind_sent]))] for ind_sent in range(len(new_input))]
+    _input_mask = [[0 if new_input[ind_sent][ind_tok] == PAD_ID_BERT or input_alignement_with_raw[ind_sent][ind_tok - 1] == input_alignement_with_raw[ind_sent][ind_tok] else 1 for ind_tok in range(len(new_input[ind_sent]))] for ind_sent in range(len(new_input))]
+
     output_tokens_tensor_new = []
 
     for ind_sent in range(len(_input_mask)):
@@ -43,10 +51,14 @@ def get_bpe_label_word_level_task(label, batch, input_tokens_tensor, input_align
         output_tokens_tensor_new.append(output_tokens_tensor_new_ls)
 
     output_tokens_tensor = torch.Tensor(output_tokens_tensor_new).long()
-    input_mask = torch.Tensor(_input_mask).long()
+    head_mask = torch.Tensor(_input_mask).long()
     input_tokens_tensor = torch.Tensor(new_input).long()
+    if use_gpu:
+        head_mask = head_mask.cuda()
+        input_tokens_tensor = input_tokens_tensor.cuda()
+        output_tokens_tensor = output_tokens_tensor.cuda()
 
-    return output_tokens_tensor, input_mask, input_tokens_tensor
+    return output_tokens_tensor, head_mask, input_tokens_tensor
 
 
 def get_label_per_bpe(tasks, batch, input_tokens_tensor, input_alignement_with_raw, use_gpu, tasks_parameters):
@@ -106,17 +118,21 @@ def get_label_per_bpe(tasks, batch, input_tokens_tensor, input_alignement_with_r
 
         if use_gpu:
             output_tokens_tensor_aligned = output_tokens_tensor_aligned.cuda()
+            input_tokens_tensor = input_tokens_tensor.cuda()
+            for lab in label_per_task:
+                label_per_task[lab] = label_per_task[lab].cuda()
+
 
     else:
+        head_masks = OrderedDict()
         for task in tasks:
             for task_batch_name in tasks_parameters[task]["label"]:
                 task_batch = eval("batch.{}".format(task_batch_name))
                 # we handle all word level tasks in the same way
                 assert tasks_parameters[task]["prediction_level"] == "word", "ERROR only word level task supported here so far"
                 if tasks_parameters[task]["prediction_level"] == "word":
-                    output_tokens_tensor, input_mask, input_tokens_tensor = \
-                        get_bpe_label_word_level_task(task_batch, batch, input_tokens_tensor, input_alignement_with_raw)
-
+                    output_tokens_tensor, head_mask, input_tokens_tensor = get_bpe_label_word_level_task(task_batch, batch, input_tokens_tensor, input_alignement_with_raw, use_gpu)
+                    head_masks[task] = head_mask
                     output_tokens_tensor_aligned = output_tokens_tensor[:, : input_tokens_tensor.size(1)]
                     output_tokens_tensor_aligned = output_tokens_tensor_aligned.contiguous()
                     if use_gpu:
@@ -130,10 +146,6 @@ def get_label_per_bpe(tasks, batch, input_tokens_tensor, input_alignement_with_r
     token_type_ids = torch.zeros_like(input_tokens_tensor)
 
     if use_gpu:
-        input_mask = input_mask.cuda()
-        input_tokens_tensor = input_tokens_tensor.cuda()
         token_type_ids = token_type_ids.cuda()
-        for lab in label_per_task:
-            label_per_task[lab] = label_per_task[lab].cuda()
 
-    return input_mask, input_tokens_tensor, token_type_ids, label_per_task
+    return head_masks, input_tokens_tensor, token_type_ids, label_per_task

model/bert_tools_from_core_code/modeling.py
@@ -916,7 +916,7 @@ class BertGraphHead(nn.Module):
         self.pad_index = pad_index
         self.unk_index = unk_index
 
-    def forward(self, x, mask=None):
+    def forward(self, x, head_mask=None):
         # apply MLPs to the BiLSTM output states
         arc_h = self.mlp_arc_h(x)
         arc_d = self.mlp_arc_d(x)
@@ -928,9 +928,11 @@ class BertGraphHead(nn.Module):
         # [batch_size, seq_len, seq_len, n_rels]
         s_labels = self.rel_attn(rel_d, rel_h).permute(0, 2, 3, 1)
 
-        if mask is not None:
+        if head_mask is not None:
+            # NB ? : is it necessary : as we only keep
             # set the scores that exceed the length of each sentence to -inf
-            s_heads.masked_fill_(~mask.unsqueeze(1), float('-inf'))
+            head_mask = head_mask.byte()
+            s_heads.masked_fill_(~head_mask.unsqueeze(1), float('-inf'))
         else:
             print("MODEL : ignoring mask ")
 
@@ -979,9 +981,11 @@ class BertMultiTask(BertPreTrainedModel):
             else:
                 self.head[task] = eval(self.task_parameters[task]["head"])(config, num_labels=self.num_labels_dic["pos"], dropout_classifier=0.1)
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, head_masks=None):
         if labels is None:
             labels = OrderedDict()
+        if head_masks is None:
+            head_masks = OrderedDict()
         logits_dict = OrderedDict()
         loss_dict = OrderedDict()
         # sanity check the labels : they should all be in
@@ -993,10 +997,11 @@ class BertMultiTask(BertPreTrainedModel):
                                        attention_mask=attention_mask,
                                        output_all_encoded_layers=self.layer_wise_attention is not None)
         for task in self.tasks:
-            logits_dict[task] = self.head[task](sequence_output)
+            logits_dict[task] = self.head[task](sequence_output, head_mask=head_masks.get(task, None))
             # handle several labels at output (e.g  parsing)
             n_pred = len(list(logits_dict[task]))
-            assert n_pred == len(self.task_parameters[task]["label"]), "ERROR : not as many labels as prediction for task {} : {} vs {} ".format(task, self.task_parameters[task]["label"], logits_dict[task])
+            assert n_pred == len(self.task_parameters[task]["label"]), \
+                "ERROR : not as many labels as prediction for task {} : {} vs {} ".format(task, self.task_parameters[task]["label"], logits_dict[task])
             logits_dict = self.rename_multi_modal_task_logits(labels=self.task_parameters[task]["label"], logits_dict=logits_dict, task=task, n_pred=n_pred)
             for label_task in self.task_parameters[task]["label"]:
                 # HANDLE HERE MULTI MODAL TASKS
@@ -1132,7 +1137,7 @@ class BertForMaskedLM(BertPreTrainedModel):
     def forward(self, input_ids,
                 input_token_mask=None, token_type_ids=None, attention_mask=None,
                 masked_lm_labels=None, labels=None, labels_task_2=None, labels_n_masks=None,
-                multi_task_loss_ponderation=None,
+                multi_task_loss_ponderation=None, head_mask=None,
                 aggregating_bert_layer_mode=None, output_all_encoded_layers=False,
                 mask_token_index=None):
 